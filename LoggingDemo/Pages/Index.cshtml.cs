cd0cbe8332305f83f58c0e2ccca157fab92ba506
JamesRobertLundberg <JamesRobertLundberg@hotmail.com>
2020-07-20 12:41:02 -0700

    Add project files.

---
+++ LoggingDemo/Pages/Index.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LoggingDemo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            _logger.LogTrace("Log Trace");
+            _logger.LogDebug("Log Debug");
+            _logger.LogInformation("Log Information");
+            _logger.LogError("Log Error");
+            _logger.LogCritical("Log Critical");
+        }
+
+        public void OnPostStartButton(int sessionCount)
+        {
+            ViewData["msg"] = $"Your Query for {sessionCount} is processed";
+        }
+        public void OnPostStopButton(int sessionCount)
+        {
+            ViewData["msg"] = $"Your Query for {sessionCount} is processed";
+        }
+        public void OnPostHistoryButton(int sessionCount)
+        {
+            ViewData["msg"] = $"Your Query for {sessionCount} is processed";
+        }
+    }
+}
